5a7c1cd4722e063b8655d12a49a6231d11f8e183
Anurag <Anurag@.com>
2026-01-07 15:51:23 +0530

    new-update

--- terratf/main.tf
+++ terratf/main.tf
@@ -5,3 +5,15 @@ provider "aws" {
 module "my-vpc-module" {
     source = "./module/vpc"
 }
+
+
+terraform {
+  backend "s3" {
+    bucket         = "s3-buck-14"
+    key            = "prod/terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
+    use_lockfile = true
+    dynamodb_table = "db-table-14"
+  }
+}
